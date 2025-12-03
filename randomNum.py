08c9fd113cb50bacf38c77e373b64f004ea462c7
Srivatsan B <srivatsanloq@outlook.com>
2025-12-03 12:22:53 +0530

    Add files via upload

---
+++ randomNum.py
@@ -0,0 +1,4 @@
+import random
+x = float(input("Enter the starting value of the random range: "))
+y = float(input("Enter the ending value of the random range: "))
+print(f"Random number generated between {x} and {y} is: {random.randint(x,y)}")
