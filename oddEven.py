a81abc09fb9955001bf1ce85305fd4989753181e
Srivatsan B <srivatsanloq@outlook.com>
2025-12-05 16:45:33 +0530

    Add files via upload

---
+++ oddEven.py
@@ -0,0 +1,6 @@
+oddEven = int(input("Enter a number: "))
+if oddEven%2 == 0:
+    print(f"The number {oddEven} is an even number.")
+else:
+    print(f"The number {oddEven} is an odd number.")
+    
